47fe694276f3e06656f1ac8ea8dff114ec8d45f5
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-07-11 10:29:36 -0300

    rotas parte visual da nota concluida,  criptografia de rotas em andamento

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,21 +8,11 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
    public function index(){
-      // carregando as notas do usuario 
+      
       $id = session('user.id');
-      $user = User::find($id)->toArray();
       $notes = User::find($id)->notes()->get()->toArray();
 
-      //abaixo a tela vai mostrar dois arrrays 
-      // 1 com os dados  do usuario 
-      // 2 com as notas que pertencem a ele 
-
-      echo'<pre>';
-      print_r($user);
-      print_r($notes);
-      
-      die();
-      return view('home');
+      return view('home',['notes' => $notes]);
    }
 
    public function newNote(){
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,23 +5,34 @@
             <div class="col">
 
                 @include('top_bar')
+                  {{-- se a ccontagem de notas for igual a zero essa parte vai aparecer  --}}
+                  @if(count($notes) == 0)
 
-                 <div class="row mt-5">
+                    <div class="row mt-5">
                     <div class="col text-center">
                         <p class="display-6 mb-5 text-secondary opacity-50">You have no notes available!</p>
                         <a href="{{ route('new') }}" class="btn btn-secondary btn-lg p-3 px-5">
                             <i class="fa-regular fa-pen-to-square me-3"></i>Create Your First Note
                         </a>
                     </div>
-                </div>
+                    </div>
+
+                  @else
+
+                        {{--Se existirem notas essa parte aparece  --}}
 
 
-                 <!-- notes are available -->
-                <div class="d-flex justify-content-end mb-3">
-                    <a href="{{ route('new') }}" class="btn btn-secondary px-3">
-                        <i class="fa-regular fa-pen-to-square me-2"></i>New Note
-                    </a>
-                </div>
+                        
+                        <div class="d-flex justify-content-end mb-3">
+                            <a href="{{ route('new') }}" class="btn btn-secondary px-3">
+                                <i class="fa-regular fa-pen-to-square me-2"></i>New Note
+                            </a>
+                        </div>
+                       @foreach($notes as $note)
+                            @include('note')
+                      @endforeach
+
+                  @endif
 
                
 
